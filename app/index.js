9b0e2d2f9c0d0fa92e490ce33ee7bbcc0606d28f
tardigrada777 <ntwrite@gmail.com>
2022-08-19 21:28:18 +0300

    feat: small improvements, don't write to notion if isbn already cached

--- app/index.js
+++ app/index.js
@@ -2,27 +2,36 @@ import { loadRows, setBookPrice } from '../lib/notion.js';
 import { getBookAverageMarketPrice } from '../lib/web.js';
 import { getOrThrow } from '../lib/config.js';
 import CliProgress, { Presets } from 'cli-progress';
+import { cache } from '../lib/cache.js';
 
 const progress = new CliProgress.SingleBar({}, Presets.shades_classic);
 
 const main = async () => {
   const id = getOrThrow('NOTION_DB_ID');
+
   console.info('Loading db rows...');
   const rows = await loadRows(id);
+
   console.info('Finding price and updating db records...');
   progress.start(rows.length);
   let currentRow = 0;
+
   // for each row in table
   // get isbn and pubDate
   for await (const row of rows) {
     // load actual average price
     const price = await getBookAverageMarketPrice(row.isbn, new Date(row.pubYear, 1));
-    // write price into row
-    await setBookPrice(row.pageId, price);
+    
+    if (!cache.getSync(row.isbn)) {
+      // write price into row
+      await setBookPrice(row.pageId, price);
+    }
+
     // update progress
     currentRow += 1;
     progress.update(currentRow);
   }
+
   progress.stop();
 };
 
